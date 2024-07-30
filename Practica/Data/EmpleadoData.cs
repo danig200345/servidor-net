2e0c24da1d30cd54155ec12321e4e9ebdf48c8a7
daniel gomez <danielg200345@gmail.com>
2024-07-29 20:13:14 -0600

    commit

---
+++ Practica/Data/EmpleadoData.cs
@@ -0,0 +1,171 @@
+﻿using Practica.Server.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Practica.Server.Data
+{
+    public class EmpleadoData
+    {
+        private readonly string conexion;
+
+        public EmpleadoData(IConfiguration configuration)
+        {
+            conexion = configuration.GetConnectionString("CadenaSQL")!;
+        }
+        public async Task<List<Empleado>> Lista()
+        {
+            List<Empleado> lista = new List<Empleado>();
+
+            using (var con = new SqlConnection(conexion))
+            {
+                await con.OpenAsync();
+                SqlCommand cmd = new SqlCommand("sp_ListaEmpleados", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        lista.Add(new Empleado
+                        {
+                            IdEmpleado = Convert.ToInt32(reader["IdEmpleado"]),
+                            NombreCompleto = reader["NombreCompleto"].ToString(),
+                            Correo = reader["Correo"].ToString(),
+                            Sueldo = Convert.ToDecimal(reader["Sueldo"]),
+                            FechaDeContrato = reader["FechaDeContrato"].ToString(),
+
+                        });
+                    }
+                }
+
+            }
+            return lista;
+
+        }
+
+
+        public async Task<Empleado> Obtener(int Id)
+        {
+            Empleado objeto = new Empleado();
+
+            using (var con = new SqlConnection(conexion))
+            {
+                await con.OpenAsync();
+                SqlCommand cmd = new SqlCommand("sp_ObtenerEmpleado", con);
+                cmd.Parameters.AddWithValue("@IdEmpleado", Id);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        objeto = new Empleado
+                        {
+                            IdEmpleado = Convert.ToInt32(reader["IdEmpleado"]),
+                            NombreCompleto = reader["NombreCompleto"].ToString(),
+                            Correo = reader["Correo"].ToString(),
+                            Sueldo = Convert.ToDecimal(reader["Sueldo"]),
+                            FechaDeContrato = reader["FechaDeContrato"].ToString(),
+
+                        };
+                    }
+                }
+
+            }
+            return objeto;
+
+        }
+
+        public async Task<bool> Crear(Empleado objeto)
+        {
+            bool respuesta = true;
+
+            using (var con = new SqlConnection(conexion))
+            {
+                
+                SqlCommand cmd = new SqlCommand("sp_CrearEmpleado", con);
+                cmd.Parameters.AddWithValue("@NombreCompleto", objeto.NombreCompleto);
+                cmd.Parameters.AddWithValue("@Correo", objeto.Correo);
+                cmd.Parameters.AddWithValue("@Sueldo", objeto.Sueldo);
+                cmd.Parameters.AddWithValue("@FechaDeContrato", objeto.FechaDeContrato);
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    await con.OpenAsync();
+
+                     respuesta = await cmd.ExecuteNonQueryAsync() > 0 ? true: false;
+                }
+                catch
+                {
+                    respuesta = false;
+                }
+            
+
+            }
+            return respuesta;
+
+        }
+
+        public async Task<bool> Editar(Empleado objeto)
+        {
+            bool respuesta = true;
+
+            using (var con = new SqlConnection(conexion))
+            {
+
+                SqlCommand cmd = new SqlCommand("sp_EditarEmpleado", con);
+                cmd.Parameters.AddWithValue("@IdEmpleado", objeto.IdEmpleado);
+                cmd.Parameters.AddWithValue("@NombreCompleto", objeto.NombreCompleto);
+                cmd.Parameters.AddWithValue("@Correo", objeto.Correo);
+                cmd.Parameters.AddWithValue("@Sueldo", objeto.Sueldo);
+                cmd.Parameters.AddWithValue("@FechaDeContrato", objeto.FechaDeContrato);
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    await con.OpenAsync();
+
+                    respuesta = await cmd.ExecuteNonQueryAsync() > 0 ? true : false;
+                }
+                catch
+                {
+                    respuesta = false;
+                }
+
+
+            }
+            return respuesta;
+
+        }
+
+        public async Task<bool> Eliminar(int Id)
+        {
+            bool respuesta = true;
+
+            using (var con = new SqlConnection(conexion))
+            {
+
+                SqlCommand cmd = new SqlCommand("sp_EliminarEmpleado", con);
+                cmd.Parameters.AddWithValue("@IdEmpleado", Id);
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    await con.OpenAsync();
+
+                    respuesta = await cmd.ExecuteNonQueryAsync() > 0 ? true : false;
+                }
+                catch
+                {
+                    respuesta = false;
+                }
+
+
+            }
+            return respuesta;
+
+        }
+
+    }
+}
+
+
---
+++ Practica/Models/Empleado.cs
@@ -0,0 +1,13 @@
+﻿namespace Practica.Server.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string? NombreCompleto { get; set; }
+        public string? Correo { get; set; }
+
+        public decimal Sueldo { get; set; }
+
+        public string? FechaDeContrato { get; set; }
+    }
+}
